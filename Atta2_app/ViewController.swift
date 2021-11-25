d6bdd7f46ab4cf8331d9bc65d0d9d09d5362ea0b
Alisa0908 <alisa0908piano@gmail.com>
2021-11-25 11:57:50 +0900

    Initial Commit

---
+++ Atta2_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Atta2_app
+//
+//  Created by 松尾有紗 on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
